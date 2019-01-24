c2a529455669bb9ac001fa54329ec8325f61f702
Chad Towle <chad@towlebooth.com>
2019-01-24 15:54:25 -0600

    Using jwt token in user login

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://chad:Redav765@ds058548.mlab.com:58548/mern_devconnector'
+    mongoURI: 'mongodb://chad:Redav765@ds058548.mlab.com:58548/mern_devconnector',
+    secretOrKey: 'secret'
 }
