f9555b559698a75a12f75b984cf0be68804f7d2e
Chad Towle <chad@towlebooth.com>
2019-01-26 11:59:56 -0600

    Created current user profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,21 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
 
-// @route GET api/profile
-// @desc Get all 
-// @access Public
-/*
-router.get('/', (req, res) => {
-    MovieNight.find()
-        .sort({ date: -1 })  //descending
-        .then(movieNights => res.json(movieNights))
-});
-*/
+// Load Profile model
+const Profile = require('../../models/Profile');
+
+// Load User model
+const User = require('../../models/User');
 
 // @route GET api/profile/test
 // @desc Tests profile route
 // @access Public
 router.get('/test', (req, res) => res.json({msg: "Profile Works"})
 );
+
+// @route GET api/profile
+// @desc Get current user's profile 
+// @access Private
+router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+    const errors = {};
+    Profile.findOne({ user: req.user.id })
+        .then(profile => {
+            if (!profile) {
+                errors.noprofile = 'There is no profile for this user';
+                return res.status(400).json(errors);
+            }
+            res.json(profile);
+        })
+        .catch(err => res.status(404).json(err));
+});
+
 
 module.exports = router;
