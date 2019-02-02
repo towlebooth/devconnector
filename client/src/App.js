3fd7d88b723decc852e2cf57fe50423503d94b3b
Chad Towle <chad@towlebooth.com>
2019-02-02 14:54:04 -0600

    Started Profile & dashboard and added private route

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
-import { setCurrentUser } from './actions/authActions';
-import { logoutUser } from './actions/authActions';
+import { setCurrentUser, logoutUser } from './actions/authActions';
+import { clearCurrentProfile } from './actions/profileActions';
 import { Provider } from 'react-redux';
 import store from './store';
+import PrivateRoute from './components/common/PrivateRoute';
 
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Dashboard from './components/dashboard/Dashboard';
 
 import './App.css';
 
@@ -27,8 +29,9 @@ if (localStorage.jwtToken) {
   // check for expired token
   const currentTime = Date.now() / 1000;
   if(decoded.exp < currentTime) {
-    store.dispatch(logoutUser());    
-    // TODO: clear current profile
+    store.dispatch(logoutUser());
+    // clear current profile
+    store.dispatch(clearCurrentProfile());
     // redirect to login
     window.location.href = '/login';
   }
@@ -45,6 +48,9 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={ Register } />
               <Route exact path="/login" component={ Login } />
+              <Switch>
+                  <PrivateRoute exact path="/dashboard" component={ Dashboard } />
+              </Switch>
             </div>
             <Footer></Footer>
           </div>
